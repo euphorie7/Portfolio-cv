2bdd82c50911956773833d09b3122a387e56cda3
euphorie <laounihamza555@gmail.com>
2025-06-26 00:22:02 +0200

    push all project files

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
+  eslint: {
+    ignoreDuringBuilds: true,   // 👈 coupe ESLint en prod
+  },
 };
 
 export default nextConfig;
-
