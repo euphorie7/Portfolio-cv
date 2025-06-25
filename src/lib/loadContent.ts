045476ccb4d465f717b9d3386874b3414dd49bb8
euphorie <laounihamza555@gmail.com>
2025-06-26 00:57:18 +0200

    fix: ESLint bypass

--- src/lib/loadContent.ts
+++ src/lib/loadContent.ts
@@ -3,7 +3,9 @@ import fs from 'node:fs/promises';
 import {parseStringPromise} from 'xml2js';
 
 export async function getContent(locale: string) {
-  const xml = await fs.readFile('langues/content.xml', 'utf8');
+  // ✅ remplace par un fetch :
+const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || ''}/langues/content.xml`);
+const xml = await res.text();
   const data: any = await parseStringPromise(xml);
   return data.portfolio.lang.find((l: any) => l.$.code === locale);
 }
