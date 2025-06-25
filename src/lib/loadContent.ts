92233c824617a4d91795600678b1efa116fbb35e
euphorie <laounihamza555@gmail.com>
2025-06-26 01:05:26 +0200

    fix 7

--- src/lib/loadContent.ts
+++ src/lib/loadContent.ts
@@ -3,9 +3,9 @@ import fs from 'node:fs/promises';
 import {parseStringPromise} from 'xml2js';
 
 export async function getContent(locale: string) {
-  // ✅ remplace par un fetch :
-const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || ''}/langues/content.xml`);
-const xml = await res.text();
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://portfolio-cv.vercel.app'; // remplace par ton URL
+  const res = await fetch(`${baseUrl}/langues/content.xml`);
+  const xml = await res.text();
   const data: any = await parseStringPromise(xml);
   return data.portfolio.lang.find((l: any) => l.$.code === locale);
 }
